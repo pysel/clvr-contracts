2f88b83523799c9143d42ad7b962f49f0a9642d4
Ruslan Akhtariev <rakhtariev@icloud.com>
2024-11-26 14:44:58 -0800

    comment out old clvr test

--- test/ClvrModel.t.sol
+++ test/ClvrModel.t.sol
@@ -17,33 +17,28 @@ contract ClvrModelTest is Test {
     function setUp() public {
         model = new ClvrModel(100e18, 100e18);
     }
-    /*
-    Box::new(Trade::new(size(5), TradeDirection::Sell)),
-    Box::new(Trade::new(size(10), TradeDirection::Buy)),
-    Box::new(Trade::new(size(2), TradeDirection::Sell)),
-     */
-    function testModel() public {
-        ClvrHook.SwapParamsExtended[] memory o = new ClvrHook.SwapParamsExtended[](4);
-        o[0] = ClvrHook.SwapParamsExtended(address(0), address(0), IPoolManager.SwapParams(false, 0, 0)); // first one is mock (address = 0)
-        o[3] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(BUY, -10e18, 0));
-        o[1] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -5e18, 0)); 
-        o[2] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -2e18, 0)); 
+    // function testModel() public {
+    //     ClvrHook.SwapParamsExtended[] memory o = new ClvrHook.SwapParamsExtended[](4);
+    //     o[0] = ClvrHook.SwapParamsExtended(address(0), address(0), IPoolManager.SwapParams(false, 0, 0)); // first one is mock (address = 0)
+    //     o[3] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(BUY, -10e18, 0));
+    //     o[1] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -5e18, 0)); 
+    //     o[2] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -2e18, 0)); 
 
-        ClvrHook.SwapParamsExtended[] memory expected = new ClvrHook.SwapParamsExtended[](4);
-        expected[0] = ClvrHook.SwapParamsExtended(address(0), address(0), IPoolManager.SwapParams(false, 0, 0));
-        expected[1] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -2e18, 0));
-        expected[2] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -5e18, 0));
-        expected[3] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(BUY, -10e18, 0));
+    //     ClvrHook.SwapParamsExtended[] memory expected = new ClvrHook.SwapParamsExtended[](4);
+    //     expected[0] = ClvrHook.SwapParamsExtended(address(0), address(0), IPoolManager.SwapParams(false, 0, 0));
+    //     expected[1] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -2e18, 0));
+    //     expected[2] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(SELL, -5e18, 0));
+    //     expected[3] = ClvrHook.SwapParamsExtended(address(1), address(1), IPoolManager.SwapParams(BUY, -10e18, 0));
 
-        uint256 gas = gasleft();
+    //     uint256 gas = gasleft();
 
-        o = model.clvrReorder(1e18, o, 100e18, 100e18);
+    //     o = model.clvrReorder(1e18, o, 100e18, 100e18);
 
-        console.log("Gas used: ", gas - gasleft());
+    //     console.log("Gas used: ", gas - gasleft());
 
-        for (uint256 i = 0; i < o.length; i++) {
-            assertEq(o[i].recepient, expected[i].recepient);
-            assertEq(o[i].params.amountSpecified, expected[i].params.amountSpecified);
-        }
-    }
+    //     for (uint256 i = 0; i < o.length; i++) {
+    //         assertEq(o[i].recepient, expected[i].recepient);
+    //         assertEq(o[i].params.amountSpecified, expected[i].params.amountSpecified);
+    //     }
+    // }
 }
